76b85367660aec09d05713ce12cd9b16f58d1d52
mikemore <mikeandmore@gmail.com>
2010-02-18 00:00:41 -0800

    add some comments

--- src/ime-core/imi_context.cpp
+++ src/ime-core/imi_context.cpp
@@ -200,6 +200,8 @@ void CIMIContext::_forwardSingleSyllable (unsigned i, unsigned j, TSyllable syll
         bool added_from_sysdict = false;
 
         if (lxst.m_pPYNode) {
+            // try to match a word from lattice i to lattice j
+            // and if match, we'll count it as a new lexicon on lattice j
             pn = m_pPinyinTrie->transfer (lxst.m_pPYNode, syllable);
             if (pn) {
                 added_from_sysdict = true;
@@ -210,11 +212,14 @@ void CIMIContext::_forwardSingleSyllable (unsigned i, unsigned j, TSyllable syll
         }
 
         if (m_pUserDict && lxst.m_syls.size() < MAX_USRDEF_WORD_LEN) {
+            // try to match a word from user dict
             CSyllables syls = lxst.m_syls;
             syls.push_back (syllable);
             std::vector<CPinyinTrie::TWordIdInfo> words;
             m_pUserDict->getWords (syls, words);
             if (!words.empty() || !added_from_sysdict) {
+                // even if the words is empty we'll add a fake lexicon
+                // here. This helps _saveUserDict detect new words.
                 TLexiconState new_lxst = TLexiconState (lxst.m_start, lxst.m_syls, words, isFuzzy);
                 new_lxst.m_syls.push_back (syllable);
                 fr.m_lexiconStates.push_back (new_lxst);
@@ -222,6 +227,7 @@ void CIMIContext::_forwardSingleSyllable (unsigned i, unsigned j, TSyllable syll
         }
     }
 
+    // last, create a lexicon for single character with only one syllable
     pn = m_pPinyinTrie->transfer (syllable);
     if (pn) {
         CSyllables syls;

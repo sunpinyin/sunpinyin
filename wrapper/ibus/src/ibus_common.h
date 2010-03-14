a03d77ddd378d8ecd76e5873292f320e2b7a4f7f
Kov Chai <tchaikov@gmail.com>
2010-03-14 17:58:40 +0800

    forget to add the missing header

---
+++ wrapper/ibus/src/ibus_common.h
@@ -0,0 +1,15 @@
+#include <ibus.h>
+#include "pointer.h"
+
+
+namespace ibus {
+    typedef Pointer<IBusText> Text;
+    typedef Pointer<IBusProperty> Property;
+    typedef Pointer<IBusPropList> PropList;
+    typedef Pointer<IBusEngine> Engine;
+    typedef Pointer<IBusLookupTable> LookupTable;
+    typedef Pointer<IBusBus> Bus;
+    typedef Pointer<IBusConfig> Config;
+    typedef Pointer<IBusComponent> Component;
+    typedef Pointer<IBusFactory> Factory;
+}

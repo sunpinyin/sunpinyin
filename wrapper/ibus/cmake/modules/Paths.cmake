5f6301c500594bcbe5c0715225584da912e48ac1
Kov Chai <tchaikov@gmail.com>
2010-07-28 03:08:10 +0800

    fix issue#176: fix the substitudes in .in files

--- wrapper/ibus/cmake/modules/Paths.cmake
+++ wrapper/ibus/cmake/modules/Paths.cmake
@@ -3,10 +3,10 @@ if(NOT DEFINED LIBEXEC_DIR)
 endif()
 add_definitions(-DLIBEXECDIR="${LIBEXEC_DIR}")
 
+set(INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 set(DATA_DIR "${CMAKE_INSTALL_PREFIX}/share/${PACKAGE}")
 set(ICON_DIR "${DATA_DIR}/icons")
-add_definitions(-DIBUS_SUNPINYIN_ICON_DIR="${ICON_DIR}")
 
-add_definitions(-DGETTEXT_PACKAGE="${GETTEXT_PACKAGE}")
-add_definitions(-DIBUS_SUNPINYIN_LOCALEDIR="${CMAKE_INSTALL_PREFIX}/share/locale")
-
+add_definitions(-DIBUS_SUNPINYIN_ICON_DIR="${ICON_DIR}"
+                -DGETTEXT_PACKAGE="${GETTEXT_PACKAGE}"
+                -DIBUS_SUNPINYIN_LOCALEDIR="${CMAKE_INSTALL_PREFIX}/share/locale")

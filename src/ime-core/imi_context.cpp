f57ecb2479f9fe7a3c48d2ac089645c21eeaf626
Yong Sun <mail@yongsun.me>
2010-09-24 01:19:39 +0800

    reverted 47937f45ef4188b8d10e, since c5ff1a02e81db8f1d746 could guarantee the condition is always true.

--- src/ime-core/imi_context.cpp
+++ src/ime-core/imi_context.cpp
@@ -428,7 +428,7 @@ void CIMIContext::_backTraceBestPaths ()
     CLatticeStates& tail_states = m_lattice[m_tailIdx].m_latticeStates;
 
     // there must be some transfer errors
-    if (!tail_states.size())
+    if (tail_states.size() != 1)
         return;
 
     TLatticeState *bs = &(tail_states[0]);
